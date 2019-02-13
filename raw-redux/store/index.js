a8d0c51baebfe03d04cea0b7980b9bed891b9d07
griffith <griffith@intcrzyppl.com>
2019-02-13 09:43:38 +0000

    [ADD] Dispatched a few actions.

--- raw-redux/store/index.js
+++ raw-redux/store/index.js
@@ -1,5 +1,28 @@
 import {createStore} from 'redux';
 import todoApp from '../reducers';
+import {addTodo, removeTodo} from '../actions';
+
 const store = createStore(todoApp);
 
 console.log(store.getState())
+
+const unsuscribe = store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(addTodo({
+  id: 0,
+  text: 'Hello World'
+}))
+
+store.dispatch(addTodo({
+  id: 1,
+  text: 'Checking'
+}))
+
+store.dispatch(addTodo({
+  id: 2,
+  text: 'Pompiido'
+}))
+
+store.dispatch(removeTodo(0))
+store.dispatch(removeTodo(1))
+store.dispatch(removeTodo(2))
